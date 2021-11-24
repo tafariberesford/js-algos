7140313e62e8097b7be557aea143ae67885d05ad
tafariberesford <beresfordtafari@gmail.com>
2021-11-23 22:02:11 -0500

    added map method for first question

--- mapChallenges.js
+++ mapChallenges.js
@@ -2,3 +2,8 @@ const nums = [1, 3, 6, 9, 12]
 
 const squaring = nums.map(val => val ** 2)
 console.log(squaring)
+
+// function square(val) {
+//     let squared = nums.map(val => val ** 2)
+//     return squared
+// }
