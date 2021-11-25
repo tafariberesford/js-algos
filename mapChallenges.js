58406a81a9f7a36aec90265f2b26481ab0568331
tafariberesford <beresfordtafari@gmail.com>
2021-11-24 21:22:00 -0500

    added one more to challenge 1

--- mapChallenges.js
+++ mapChallenges.js
@@ -1,7 +1,6 @@
 const nums = [1, 3, 6, 9, 12]
 
 const squaring = nums.map(val => val ** 2)
-console.log(squaring)
 
 // function square(val) {
 //     let squared = nums.map(val => val ** 2)
@@ -12,3 +11,7 @@ console.log(squaring)
 //     let square = nums.map(val => val ** 2)
 //     return square
 // }
+
+const squared = nums.map(function(val) {
+  return val ** 2
+})
