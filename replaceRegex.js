a28d69754d95139c8bc924c2fe926001c9d9249b
tafariberesford <beresfordtafari@gmail.com>
2021-12-05 23:37:14 -0500

    regex

--- replaceRegex.js
+++ replaceRegex.js
@@ -2,4 +2,4 @@ function correct(string) {
   return string.replace(/0/g, "O").replace(/5/g, "S").replace(/1/g, "I");
 }
 
-console.log(correct("1 want 0ne of the skittle5."))
+// console.log(correct("1 want 0ne of the skittle5."))
