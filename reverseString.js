961a785685aec69485b7b412e81ca3d50da0144a
tafariberesford <beresfordtafari@gmail.com>
2021-12-12 13:18:20 -0500

    continous practice

--- reverseString.js
+++ reverseString.js
@@ -16,3 +16,26 @@
 
 // console.log(reverse('Hello'))
 
+// * -- Solution 2
+
+// function reverseStr(str) {
+//   let reverse = ""
+  
+//   for (let i = 0; i < str.length; i++) {
+//     const char = str[i]
+//     reverse = char + reverse
+//   }
+//   return reverse
+// }
+
+// console.log(reverseStr("hello"))
+
+// ** CONTINOUS PRACTICE **
+
+// function reverseString(str) {
+//   str = str.toUpperCase()
+//   const reverse = str.split('').reverse().join('')
+//   return reverse
+// }
+
+// console.log(reverseString("Hiking"))
