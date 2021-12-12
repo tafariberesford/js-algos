4fe52650f083d1dbe477897cce988c78c6001f84
tafariberesford <beresfordtafari@gmail.com>
2021-12-12 11:57:58 -0500

    reverseString solution 1 added

---
+++ reverseString.js
@@ -0,0 +1,18 @@
+// const string = "hello"
+
+// function reverse() {
+//   const arr1 = string.split('')
+//   arr1.reverse()
+//   return arr1.join('')
+// }
+
+// console.log(reverse())
+
+// function reverse(str) {
+//   const arr1 = str.split('')
+//   arr1.reverse()
+//   return arr1.join('')
+// }
+
+// console.log(reverse('Hello'))
+
