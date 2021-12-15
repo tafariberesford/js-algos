051a69c452cfae8ae0c150f733b839c18574be2c
tafariberesford <beresfordtafari@gmail.com>
2021-12-15 14:51:28 -0500

    solution for array starting with 0 added

---
+++ findMissingNum.js
@@ -0,0 +1,31 @@
+
+// function missingNumber(nums) {
+//   let sumIfNoneMissing = nums.length
+//   let actualSum = 0
+
+//   for (let i = 0; i < nums.length; i++) {
+//     sumIfNoneMissing += i
+//     actualSum += nums[i]
+//     console.log(sumIfNoneMissing)
+//     console.log(actualSum)
+//   }
+
+//   return sumIfNoneMissing - actualSum
+// }
+
+// console.log(missingNumber([0, 1, 3]))
+
+// [0, 1, 3] find the missing number
+
+// function findMissingNum(arr) {
+//   let sumIfNoMissing = arr.length
+//   let actualSum = 0
+
+//   for (let i = 0; i < arr.length; i++) {
+//     sumIfNoMissing += i
+//     actualSum += arr[i]
+//   }
+//   return sumIfNoMissing - actualSum
+// }
+
+// console.log(findMissingNum([0, 1, 3]))
