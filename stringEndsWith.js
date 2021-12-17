fdfe6ef807b98c64f0243ce57c720c770c1d286b
tafariberesford <beresfordtafari@gmail.com>
2021-12-17 08:21:27 -0500

    added comments

--- stringEndsWith.js
+++ stringEndsWith.js
@@ -1,4 +1,6 @@
 function solution(str, ending) {
+  // Use charAt() method & str.length - 1 to access the character at the end of both arguments
+  // if statement to return true if the ending matches
   if (str.charAt(str.length - 1) == ending.charAt(ending.length - 1)) {
     return true
   } else return false
