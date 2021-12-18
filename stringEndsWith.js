c376945f1fbbd7f5ac924252037f7aa409cdbacc
tafariberesford <beresfordtafari@gmail.com>
2021-12-17 23:54:10 -0500

    solution works on hackerrank

--- stringEndsWith.js
+++ stringEndsWith.js
@@ -1,9 +1,15 @@
-function solution(str, ending) {
-  // Use charAt() method & str.length - 1 to access the character at the end of both arguments
-  // if statement to return true if the ending matches
-  if (str.charAt(str.length - 1) == ending.charAt(ending.length - 1)) {
-    return true
-  } else return false
+// function solution(str, ending) {
+//   // Use charAt() method & str.length - 1 to access the character at the end of both arguments
+//   // if statement to return true if the ending matches
+//   if (str.charAt(str.length - 1) == ending.charAt(ending.length - 1)) {
+//     return true
+//   } else return false
+// }
+
+// console.log(solution("Happy", "Day"))
+
+function solution(str, ending){
+  return str.endsWith(ending)
 }
 
-console.log(solution("Happy", "Day"))
+console.log(solution("sumo", "tsmo"))
