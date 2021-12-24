1dce88ea384b2facc8c35a2d6fb02ae96f578143
tafariberesford <beresfordtafari@gmail.com>
2021-12-24 17:31:33 -0500

    practice

--- sumUp.js
+++ sumUp.js
@@ -9,4 +9,27 @@ function count(n) {
   return sum
 }
 
-console.log(count(9))
+// console.log(count(11))
+
+// function sum(n) {
+//   let total = n.map(val => total += val)
+//   }
+// }
+
+function sumUp(arr) {
+  let total = 0
+  for (let i = 0; i < arr; i++) {
+    total += i
+  }
+  return total
+}
+
+// console.log(sumUp(1, 2))
+
+function sumUp(n) {
+  let sum = 0
+  sum = n.map(val => val += sum)
+  return sum
+}
+
+console.log(sumUp(9))
