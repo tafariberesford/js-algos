b392d4c0383cb08e944f6c5e7b299587fd535179
tafariberesford <beresfordtafari@gmail.com>
2021-12-29 23:45:15 -0500

    practice

--- array.js
+++ array.js
@@ -6,8 +6,17 @@ function factor(arr) {
   return maxNum
 }
 
-console.log(factor([1, 2, 3, 4, 5]))
+// console.log(factor([1, 2, 3, 4, 5]))
 
 // for (let i = 0; i <= arr.length; i++) {
 //   console.log(i)
 // }
+
+// -- Create a function which takes an argument - array of integers. The new array should putput the largest element from each array.
+
+function largestNums(arr) {
+  const largest = Math.max(...arr)
+  return largest
+}
+
+console.log(largestNums([1, 2, 3, 4]))
