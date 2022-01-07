f4925d2281dcacca565913e8ebf5552690f73842
tafariberesford <beresfordtafari@gmail.com>
2022-01-06 23:59:35 -0500

    practice

--- replaceRegex.js
+++ replaceRegex.js
@@ -9,3 +9,9 @@ function correction(string) {
 }
 
 // console.log(correction("I have 1. Do you have 3 or more?"))
+
+let nameArr = ["Janice", "Sally", "Jacob"]
+
+const updated = nameArr.map(letter => letter.replace(/a/g, "@"))
+
+// console.log(updated.toString())
